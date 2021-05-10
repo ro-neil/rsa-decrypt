00741840a686ee15625742b0adbeff2eaed062e2
Roneil Barrett <roneilbarrett@gmail.com>
2021-05-10 03:37:38 -0500

    Removed GET request from / route

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ from .rsa_decrypt import RSA
 # Routing for your application.
 ###
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def home():
     """Render website's home page."""
     form = DecryptionForm()
